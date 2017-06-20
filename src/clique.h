0e5a77c1fccae576bc9cd6f59cf469aee1f7dd9b
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-06-20 16:30:39 -0300

    Borrar typedefs repetidos

--- src/clique.h
+++ src/clique.h
@@ -3,9 +3,6 @@
 
 #include "graph.h"
 
-typedef std::vector<unsigned int> nodeSet;
-typedef std::vector< std::vector< bool > > adjMatrix;
-
 struct cliqueInfo {
     cliqueInfo(size_t k, unsigned int f) : nodes(k), outgoing(f) {}
     cliqueInfo(nodeSet g, unsigned int f) : nodes(g), outgoing(f) {}
