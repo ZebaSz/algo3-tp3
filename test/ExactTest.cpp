b006789a7a6ddcaba449d68033de2134e07faae3
Roni <ronibarylko@hotmail.com>
2017-06-20 06:15:41 -0300

    test

--- test/ExactTest.cpp
+++ test/ExactTest.cpp
@@ -20,3 +20,18 @@ TEST_F(ExactText, k4) {
 
     ASSERT_EQ(exactCMF(n, edges), (unsigned int)4);
 }
+
+TEST_F(ExactText, k4PlusTwoEdges) {
+    edges.push_back({0,1});
+    edges.push_back({0,2});
+    edges.push_back({0,3});
+    edges.push_back({1,2});
+    edges.push_back({1,3});
+    edges.push_back({2,3});
+    edges.push_back({3,4});
+    edges.push_back({3,5});
+
+    n = 6;
+
+    ASSERT_EQ(exactCMF(n, edges), (unsigned int)6);
+}
