afe4503e892e493c8c0b0c7405fa0e6737c2f513
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-06-20 21:05:17 -0300

    Agregar .out faltantes y tests enormes

--- test/ExactTest.cpp
+++ test/ExactTest.cpp
@@ -41,3 +41,11 @@ TEST_F(ExactText, allSmall) {
         Utils::log(DEBUG, "Passed test %d", i);
     }
 }
+
+TEST_F(ExactText, allHuge) {
+    std::vector<testcase> tests(getTests(false));
+    for (size_t i = 0; i < tests.size(); ++i) {
+        ASSERT_EQ(exactCMF(tests[i].input).outgoing, tests[i].output.outgoing);
+        Utils::log(DEBUG, "Passed test %d", i);
+    }
+}
