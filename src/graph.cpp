4827f4277d45c1163026bfb55293a765687b2f4e
Ignacio Tarrio <itarrio@despegar.com>
2017-06-23 19:15:42 -0300

    todos adyacentes a con listas ordenadas

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include "graph.h"
 
-bool ::Graph::allAdjacentTo(const adjMatrix &graph, const nodeSet &subclique, const unsigned int node) {
+bool ::Graph::allAdjacentTo(const adjMatrix &graph, const nodeSet &subclique, const node node) {
     for (size_t i = 0; i < subclique.size(); i++){
-        if(!graph[subclique[i]][node]) {
+        if (!graph[subclique[i]][node]) {
             return false;
         }
     }
     return true;
 }
 
-bool ::Graph::allAdjacentTo(const adjList &graph, const nodeSet &subclique, const unsigned int node) {
+bool ::Graph::allAdjacentTo(const adjList &graph, const nodeSet &subclique, const node node) {
     unsigned bool cliqueElementsFound[graph.size()];
     for (size_t i = 0; i < subclique.size(); i++) {
         cliqueElementsFound[i] = false;
@@ -24,4 +24,20 @@ bool ::Graph::allAdjacentTo(const adjList &graph, const nodeSet &subclique, cons
         }
     }
     return true;
+}
+
+bool ::Graph::allAdjacentToOrd(const adjList &graph, const nodeSet &subclique, const node node) {
+    std::vector<unsigned int>::const_iterator it;
+    size_t cliqueNodeIndex = 0;
+    for (it = graph[node].begin(); it != graph[node].end(); ++it) {
+        if (*it > subclique[cliqueNodeIndex]) {
+            return false;
+        } else if (*it == subclique[cliqueNodeIndex]) {
+            cliqueNodeIndex++;
+            if (cliqueNodeIndex == subclique.size()) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
--- src/graph.h
+++ src/graph.h
@@ -23,16 +23,25 @@ struct graphInfo {
 namespace Graph {
 
     /**
-     * @info Esta es la nueva isClique, podemos mejorar la de lista si esta ordenada
+     * @info Esta es la nueva isClique
      * @param graph
      * @param subclique
      * @param node
      * @return Si el nodo es adyacente a todos los nodos de la subclique
      * @complexity O(|subclique|))
      */
+    bool allAdjacentTo(const adjMatrix& graph, const nodeSet& subclique, const node node);
+    bool allAdjacentTo(const adjList& graph, const nodeSet& subclique, const node node);
 
-    bool allAdjacentTo(const adjMatrix& graph, const nodeSet& subclique, const unsigned int node);
-    bool allAdjacentTo(const adjList& graph, const nodeSet& subclique, const unsigned int node);
+    /**
+     * @precondition La lista de adyacencias y el la lista de la subclique tiene que estar ordenado
+     * @param graph
+     * @param subclique
+     * @param node
+     * @return Si el nodo es adyacente a todos los nodos de la subclique
+     * @complexity O(|adj(node)|))
+     */
+    bool allAdjacentToOrd(const adjList &graph, const nodeSet &subclique, const node node);
 
 }
 #endif //ALGO3_TP2_GRAPH_H
