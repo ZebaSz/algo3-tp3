6c4f34b8415f0a4ab85f388398730e72d799eb66
Roni <ronibarylko@hotmail.com>
2017-06-25 00:59:49 -0300

    local siendo local greedy fandom

--- src/localsearch.cpp
+++ src/localsearch.cpp
@@ -50,8 +50,7 @@ cliqueInfo findBestNeighborSolution(const adjList &adjacencyList, cliqueInfo par
     cliqueInfo add = localAdd(adjacencyList, partialClique);
     cliqueInfo remove = localRemove(adjacencyList, partialClique);
     cliqueInfo swap = localSwap(adjacencyList, partialClique);
-    cliqueInfo swap2 = localSwap2(adjacencyList, partialClique);
-
+    cliqueInfo swap2= localSwap2(adjacencyList, partialClique);
     if (add.outgoing >= remove.outgoing && add.outgoing >= swap.outgoing && add.outgoing >= swap2.outgoing) {
         return add;
     } else if (remove.outgoing >= add.outgoing && remove.outgoing >= swap.outgoing && remove.outgoing >= swap2.outgoing) {
@@ -100,7 +99,7 @@ cliqueInfo localRemove(const adjList &adjacencyList, cliqueInfo partialClique) {
 cliqueInfo localSwap(const adjList &adjacencyList, cliqueInfo partialClique) {
     std::vector<node>::iterator toRemove;
     node toAdd;
-    int best = 0;
+    int best = -1000;
 
     nodeSet toConsider;
     for (node n = 0; n < adjacencyList.size(); n++) {
@@ -108,18 +107,20 @@ cliqueInfo localSwap(const adjList &adjacencyList, cliqueInfo partialClique) {
             toConsider.push_back(n);
         }
     }
-
-    for (auto it = partialClique.nodes.begin(); it != partialClique.nodes.end(); ++it) {
+    for (int i = 0; i < partialClique.nodes.size(); i++){
         for (auto ot = toConsider.begin(); ot != toConsider.end(); ++ot) {
-            int status = (int) adjacencyList[*ot].size() - (int) adjacencyList[*it].size();
-            if (status > best && Graph::allAdjacentToExceptFor(adjacencyList, partialClique.nodes, *ot, *it)) {
+            int status = (int) adjacencyList[*ot].size() - (int) adjacencyList[i].size();
+            cliqueInfo testClique = partialClique;
+            testClique.nodes.erase(testClique.nodes.begin() + i);
+            if (status > best && Graph::allAdjacentTo(adjacencyList, testClique.nodes, *ot)) {
                 best = status;
-                toRemove = it;
+                toRemove = partialClique.nodes.begin() + i;
                 toAdd = *ot;
             }
         }
     }
-    if (best > 0) {
+    if (best > -1000) {
+        partialClique.outgoing = (unsigned int)(partialClique.outgoing - adjacencyList[*toRemove].size()  + adjacencyList[toAdd].size());
         partialClique.nodes.erase(toRemove);
         partialClique.nodes.push_back(toAdd);
     }
@@ -130,7 +131,7 @@ cliqueInfo localSwap2(const adjList &adjacencyList, cliqueInfo partialClique) {
     node toRemove1, toRemove2, toAdd1, toAdd2;
     int best = 0;
 
-    if (partialClique.nodes.size() > 2) { //SE PUEDE MEJORAR PARA CASOS DE >= 2
+    //if (partialClique.nodes.size() > 2) { //SE PUEDE MEJORAR PARA CASOS DE >= 2
         for (node r1 = 0; r1 < partialClique.nodes.size() - 1; r1++) {
             for (node r2 = r1; r2 < partialClique.nodes.size(); r2++) {
                 // stillInside es un nodo que sigue estando en la clique si sacamos r1 y r2, va a hacer el primero el segundo o el tercero (dependiendo si r1 o r2 son los primeros)
@@ -140,32 +141,47 @@ cliqueInfo localSwap2(const adjList &adjacencyList, cliqueInfo partialClique) {
                 }
 
                 //Si el nodo que sigue dentro de la clique tiene al menos 2 mas nodos adyacentes que los de la clique
-                if (adjacencyList[stillInside].size() > partialClique.nodes.size() + 1) {
-                    for (auto a1 = adjacencyList[stillInside].begin(); a1 != adjacencyList[stillInside].end(); ++a1) {
-                        for (auto a2 = a1; a2 != adjacencyList[stillInside].end(); ++a2) {
-                            int status = (int) adjacencyList[*a1].size() + (int) adjacencyList[*a2].size() -  (int) adjacencyList[r1].size() - (int) adjacencyList[r2].size();
-                            if (status > best && Graph::isAdjacentTo(adjacencyList, *a1, *a2) &&
-                                    Graph::allAdjacentToExceptForDouble(adjacencyList, partialClique.nodes, *a1, *a2, r1, r2) ) {
-                                toRemove1 = r1;
-                                toRemove2 = r2;
-                                toAdd1 = *a1;
-                                toAdd2 = *a2;
+                //if (adjacencyList[partialClique.nodes[stillInside]].size() > partialClique.nodes.size() + 1) {
+                    for (node a1 = 0; a1 < adjacencyList[partialClique.nodes[stillInside]].size(); a1++){
+                        for (node a2 = a1; a1 < adjacencyList[partialClique.nodes[stillInside]].size(); a1++) {
+                            cliqueInfo testClique = partialClique;
+                            testClique.nodes.erase(testClique.nodes.begin() + r2);
+                            testClique.nodes.erase(testClique.nodes.begin() + r1);
+                            if (Graph::isAdjacentTo(adjacencyList, a1, a2) &&
+                                Graph::allAdjacentTo(adjacencyList, testClique.nodes, a1) &&
+                                Graph::allAdjacentTo(adjacencyList, testClique.nodes, a2)){
+                                testClique.nodes.push_back(a1);
+                                testClique.nodes.push_back(a2);
+                                testClique.outgoing = 0;
+                                for (int i = 0; i < testClique.nodes.size(); i++){
+                                    testClique.outgoing = (unsigned int) (testClique.outgoing + adjacencyList[testClique.nodes[i]].size() + 1 - testClique.nodes.size());
+                                }
+                                testClique = greedyHeuristic(adjacencyList, testClique);
+                                if (testClique.outgoing > best){
+                                    toRemove1 = r1;
+                                    toRemove2 = r2;
+                                    toAdd1 = a1;
+                                    toAdd2 = a2;
+                                    best = testClique.outgoing;
                             }
                         }
                     }
                 }
             }
         }
-    }
-
+    //}
     if (best > 0) {
-        partialClique.nodes.erase(std::remove(partialClique.nodes.begin(), partialClique.nodes.end(), toRemove1), partialClique.nodes.end());
-        partialClique.nodes.erase(std::remove(partialClique.nodes.begin(), partialClique.nodes.end(), toRemove2), partialClique.nodes.end());
+        partialClique.nodes.erase(partialClique.nodes.begin() + toRemove2);
+        partialClique.nodes.erase(partialClique.nodes.begin() + toRemove1);
         partialClique.nodes.push_back(toAdd1);
         partialClique.nodes.push_back(toAdd2);
-
+        partialClique.outgoing = 0;
+        for (int i = 0; i < partialClique.nodes.size(); i++){
+            partialClique.outgoing = (unsigned int) (partialClique.outgoing + adjacencyList[partialClique.nodes[i]].size() + 1 - partialClique.nodes.size());
+        }
     }
-    return partialClique;
+
+    return greedyHeuristic(adjacencyList, partialClique);
 }
 
 
